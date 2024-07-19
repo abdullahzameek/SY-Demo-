6747e41a270e6a1b4a24a115d6f6ce40baad0ea9
abdullahzameek <arz268@nyu.edu>
2024-07-20 00:22:32 +0400

    feat: added a bucnh of stuff for SY

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
-def test():
-    print('hi')
+import os
+import subprocess
+import sys 
 
+def run_scripts(name):
+    paths = {
+        "AZ" : 'some path',
+        "SY" : 'another path'
+    }
+    print(f'Using {paths[name]}')
+    for script in ['scriptA.py', 'scriptB.py', 'scriptC.py']:
+        subprocess.call(["python3", script])
 
-def new_test():
-    print('bye')
-
-def multiply_number(x,y):
-    return x*y
- 
 if __name__ == "__main__":
-    test()
+    if len(sys.argv) != 3 or sys.argv[1] != '--name':
+        print("Usage: python test.py --name <name>")
+        sys.exit(1);
+    name = sys.argv[2]
+    run_scripts(name)
